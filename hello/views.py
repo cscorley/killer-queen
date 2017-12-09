de76fd49938117e3640c3ac6a24c9e4f642ed7f2
Christopher Corley <cscorley@ua.edu>
2017-12-09 17:00:18 -0500

    demo

--- hello/views.py
+++ hello/views.py
@@ -3,11 +3,17 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
-# Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+import requests
 
+# Create your views here.
+# def index(request):
+#     # return HttpResponse('Hello from Python!')
+#     return render(request, 'index.html')
+
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 def db(request):
 
