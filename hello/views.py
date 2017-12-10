495d61d4b71b3b48f5e32921fc53513ef35ff2ab
Christopher Corley <cscorley@ua.edu>
2017-12-09 21:16:54 -0500

    generate unnamed player

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import Player
 
 import requests
 import os
@@ -20,11 +20,11 @@ def index(request):
     # return HttpResponse('<pre>' + r.text + '</pre>')
 
 def db(request):
+    player = Player()
+    player.name = 'test player'
+    player.save()
 
-    greeting = Greeting()
-    greeting.save()
+    players = Player.objects.all()
 
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
+    return render(request, 'db.html', {'players': players})
 
