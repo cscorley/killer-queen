ae3487256c590123a86556c71705bb47eb3e96b8
Christopher Corley <cscorley@ua.edu>
2017-12-09 21:22:52 -0500

    correct models for migration

--- hello/models.py
+++ hello/models.py
@@ -4,9 +4,13 @@ import trueskill
 class Player(models.Model):
     name = models.CharField('name', max_length=255)
     email = models.EmailField('contact email')
-    when = models.DateTimeField('date created', auto_now_add=True)
-    trueskill_rating_mu = models.DecimalField('TrueSkill.Rating mu parameter')
-    trueskill_rating_sigma = models.DecimalField('TrueSkill.Rating sigma parameter')
+    created = models.DateTimeField('date created', auto_now_add=True)
+    trueskill_rating_mu = models.DecimalField('TrueSkill.Rating mu parameter',
+                                              max_digits=10,
+                                              decimal_places=4)
+    trueskill_rating_sigma = models.DecimalField('TrueSkill.Rating sigma parameter',
+                                                 max_digits=10,
+                                                 decimal_places=4)
 
 
 class Team(models.Model):
@@ -24,7 +28,7 @@ class TeamMembership(models.Model):
 
 class GameResult(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
-    blue = models.ForeignKey(Team, on_delete=models.CASCADE)
-    gold = models.ForeignKey(Team, on_delete=models.CASCADE)
+    blue = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='blue_result')
+    gold = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='gold_result')
     blue_win_count = models.PositiveSmallIntegerField('Number of wins by the Blue team')
     gold_win_count = models.PositiveSmallIntegerField('Number of wins by the Gold team')
