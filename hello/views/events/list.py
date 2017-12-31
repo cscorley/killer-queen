ee097ea48317457eb87d34f9861173e94a957fce
Christopher Corley <cscorley@ua.edu>
2017-12-30 22:01:20 -0500

    actually fix issue #17 (ordering)

--- hello/views/events/list.py
+++ hello/views/events/list.py
@@ -5,7 +5,4 @@ class EventListView(ListView):
     model = Event
 
     def get_queryset(self):
-        return Event.objects.filter(is_current=False)
-
-    def get_ordering(self):
-        return "when"
+        return Event.objects.filter(is_current=False).order_by("when")
