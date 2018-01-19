7fced4bacae721e4e0192ee5249c4a8e30742ce2
Christopher Corley <cscorley@ua.edu>
2018-01-19 17:38:50 -0500

    filter players with low confidence from appearing in the public top 10 (issue #38)

--- hello/views/top_players.py
+++ hello/views/top_players.py
@@ -16,7 +16,7 @@ def top_players(request):
     if request.user.is_staff:
         bees = players[1:]
     else:
-        bees = players[1:10]
+        bees = [player for player in players if player.confidence() > 50][1:10]
 
     return render(request, 'top-players.html', {'queen': queen,
                                                 'bees': bees,
