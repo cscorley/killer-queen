427cc142c885df27cbcff67fed36e6f491f73bf9
Christopher Corley <cscorley@ua.edu>
2018-04-09 02:11:24 -0400

    add different top player sortings (issue #15)

--- hello/views/top_players.py
+++ hello/views/top_players.py
@@ -10,14 +10,26 @@ logger = logging.getLogger("hello")
 
 def top_players(request):
     players = Player.objects.all()
-    players = sorted(players, key=lambda player: player.trueskill_rating_exposure, reverse=True)
 
-    queen = players[0]
-    if request.user.is_staff:
-        bees = players[1:]
-    else:
-        bees = [player for player in players if len(player.team_set.all()) >= 3][1:10]
+    rank_queen, rank_bees = _split_queen(request, players, lambda player: player.trueskill_rating_exposure)
+    match_wins_queen, match_wins_bees = _split_queen(request, players, lambda player: player.match_wins)
+    map_wins_queen, map_wins_bees = _split_queen(request, players, lambda player: player.map_wins)
 
-    return render(request, 'top-players.html', {'queen': queen,
-                                                'bees': bees,
+    return render(request, 'top-players.html', {'rank_queen': rank_queen,
+                                                'rank_bees': rank_bees,
+                                                'match_wins_queen': match_wins_queen,
+                                                'match_wins_bees': match_wins_bees,
+                                                'map_wins_queen': map_wins_queen,
+                                                'map_wins_bees': map_wins_bees,
                                                 })
+
+def _split_queen(request, players, sorter):
+    bees = sorted(players, key=sorter, reverse=True)
+
+    queen = bees[0]
+    if request.user.is_staff:
+        bees = bees[1:]
+    else:
+        bees = [bee for bee in bees if len(bee.team_set.all()) >= 3][1:10]
+
+    return queen, bees
