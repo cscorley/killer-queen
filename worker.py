9ffae217c4e46345f06c9aa81a85d5c0eea278f4
Christopher Corley <cscorley@ua.edu>
2018-04-09 01:35:42 -0400

    load django app from worker script

--- worker.py
+++ worker.py
@@ -4,6 +4,7 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 
+
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
@@ -11,6 +12,10 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baseapp.settings")
+    import django
+    django.setup()
+
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
