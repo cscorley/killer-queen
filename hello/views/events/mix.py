6697217561d70298e7267885b34b0581034ce254
Christopher Corley <cscorley@ua.edu>
2018-08-27 00:37:09 -0400

    Require users to be active to be mixed onto teams and appare in top list

--- hello/views/events/mix.py
+++ hello/views/events/mix.py
@@ -29,7 +29,7 @@ def mix(request, event_id):
     form = MixerForm()
     alert = None
 
-    all_players: List[Player] = list(event.players.order_by('eventplayer__created'))
+    all_players: List[Player] = list(event.players.filter(user__is_active=True).order_by('eventplayer__created'))
     max_rating = max([x.trueskill_rating_exposure if x else 0 for x in all_players])
 
     if request.method == 'POST':
--- hello/views/top_players.py
+++ hello/views/top_players.py
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger("hello")
 
 def top_players(request):
-    players = Player.objects.all()
+    players = Player.objects.filter(user__is_active=True).distinct()
 
     rank_queen, rank_bees = _split_queen(request, players, lambda player: player.trueskill_rating_exposure)
     match_wins_queen, match_wins_bees = _split_queen(request, players, lambda player: player.match_wins)
@@ -42,7 +42,7 @@ def seasonal_top_players(request, season_id):
     season = Season.objects.get(pk=season_id)
 
     # players on teams in results for events this season
-    players = Player.objects.filter(team__event__season=season).distinct()
+    players = Player.objects.filter(user__is_active=True).filter(team__event__season=season).distinct()
     logger.debug(players.query)
     player_map_wins = list()
 
