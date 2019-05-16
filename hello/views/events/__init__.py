97a7860d013f3831950c3379be915b082d5d3a6d
Christopher S. Corley <cscorley@gmail.com>
2019-05-15 22:42:53 -0400

    fix dumb import

--- hello/views/events/__init__.py
+++ hello/views/events/__init__.py
@@ -1,6 +1,6 @@
 from .join import join
 from .result import result
-from .current import current
+from .current import current, current_kiosk
 from .result import result
 from .list import EventListView
 from .mix import mix
