069b382214552d37c3b793a54ed0cd0c7dd28bf6
Christopher S. Corley <cscorley@gmail.com>
2019-10-20 13:48:15 -0400

    current event goes by today

--- hello/views/events/current.py
+++ hello/views/events/current.py
@@ -1,17 +1,28 @@
 from django.shortcuts import redirect
 
 from hello.models import Event
+from datetime import date
+
+def get_todays_event() -> Event:
+    current_events = Event.objects.filter(is_current=True).order_by('when', 'pk')
+
+    for event in current_events:
+        if event.when.date == date.today():
+            return event
+
+    return None
+
 
 def current(request):
-    current_events = Event.objects.filter(is_current=True).order_by('when', 'pk')
-    if len(current_events):
-        return redirect('/events/%d/join' % current_events[0].id)
+    current_event = get_todays_event()
+    if current_event:
+        return redirect('/events/%d/join' % current_event.id)
 
     return redirect('/')
 
 def current_kiosk(request):
-    current_events = Event.objects.filter(is_current=True).order_by('when', 'pk')
-    if len(current_events):
-        return redirect('/events/%d/kiosk' % current_events[0].id)
+    current_event = get_todays_event()
+    if current_event:
+        return redirect('/events/%d/kiosk' % current_event.id)
 
     return redirect('/kiosk')
