2bf2188ca049d0ae83311954ff72916c953ef7dc
Christopher S. Corley <cscorley@gmail.com>
2019-11-03 22:34:51 -0500

    expand scope of 'current' event

--- hello/views/events/current.py
+++ hello/views/events/current.py
@@ -1,10 +1,23 @@
 from django.shortcuts import redirect
 
 from hello.models import Event
-from datetime import date
+from datetime import date, timedelta
 
 def get_todays_event() -> Event:
-    current_events = Event.objects.filter(when__date=date.today()).order_by('when', 'pk')
+    today = date.today()
+    current_events = Event.objects.filter(when__date=today).order_by('when', 'pk')
+
+    if len(current_events):
+        return current_events[0]
+
+    yesterday = today - timedelta(days=1)
+    current_events = Event.objects.filter(when__date=yesterday).order_by('when', 'pk')
+
+    if len(current_events):
+        return current_events[0]
+
+    tomorrow = today + timedelta(days=1)
+    current_events = Event.objects.filter(when__date=tomorrow).order_by('when', 'pk')
 
     if len(current_events):
         return current_events[0]
