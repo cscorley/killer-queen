d385aa46d39a6178c5d9e8265988b9db45b5e54e
Christopher Corley <cscorley@gmail.com>
2023-10-28 18:28:30 -0400

    flake + pipenv

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonEnv = pkgs.python3.withPackages (p: with p; [ pip black ]);
+      in {
+        devShell = pkgs.mkShell {
+          packages = [ pythonEnv pkgs.pipenv pkgs.postgresql ];
+        };
+      });
+}
